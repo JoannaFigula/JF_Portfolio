b3189a31510878ec60a1933cea6c9de2506eae65
joanna.figula@gmail.com <joanna.figula@gmail.com>
2020-12-01 13:01:42 +0100

    add new features to portfolioProjects

--- src/Components/PortfolioProjects/PortfolioProjects.js
+++ src/Components/PortfolioProjects/PortfolioProjects.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import country from "../../img/countrySearch.jpg";
 import things from "../../img/thingsSharing.jpg";
-import progress from "../../img/progress.jpg";
+import progress from "../../img/sitchair.png";
 import 'font-awesome/css/font-awesome.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -12,7 +12,7 @@ import { faCode } from '@fortawesome/free-solid-svg-icons'
 export default class PortfolioProjects extends Component {
     render(){
         return(
-            <section className='myWork container'>
+            <section className='portfolioProjects container'>
                 <div className="grid">
                     <figure className="effect-zoe">
                         <h2>Country <span>Search</span></h2>
@@ -22,15 +22,20 @@ export default class PortfolioProjects extends Component {
                                 <h3>Country <span>Search</span></h3>
                                 <div className="icons">
                                     <span><FontAwesomeIcon icon={faEye } /></span>
-                                    <span><a href="https://github.com/JoannaFigula/Country-Search" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faCode} /></a></span>
+                                    <span><a href="https://country-search-joannafigula-site.netlify.app/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faCode} /></a></span>
 
                                 </div>
                             </div>
-                            <p>The application that downloads basic data about the searched country.
-                                The data is collected from https://restcountries.eu - a database
-                                of information about countries around the world. The JavaScript
-                                programming language library (ReactJS) and cascading
-                                style sheets (CSS) were used.</p>
+                            <p>
+                                <span>&lt; React.js, </span>
+                                <span>HTML, </span>
+                                <span>SCSS  /&gt;</span>
+                            </p>
+                            {/*<p>The application that downloads basic data about the searched country.*/}
+                            {/*    The data is collected from https://restcountries.eu - a database*/}
+                            {/*    of information about countries around the world. The JavaScript*/}
+                            {/*    programming language library (ReactJS) and cascading*/}
+                            {/*    style sheets (CSS) were used.</p>*/}
                         </figcaption>
                     </figure>
                     <figure className="effect-zoe">
@@ -41,28 +46,42 @@ export default class PortfolioProjects extends Component {
                                 <h3>Things <span>Sharing</span></h3>
                                 <div className="icons">
                                     <span><FontAwesomeIcon icon={faEye} /></span>
-                                    <span><a href="https://github.com/JoannaFigula/Things_sharing" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faCode} /></a></span>
+                                    <span><a href="https://country-search-joannafigula-site.netlify.app/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faCode} /></a></span>
                                 </div>
                             </div>
-                            <p>The aim of the project were to create a place where everyone
-                                could give unnecessary things to trusted institutions.
-                                The project is a website with a simple form that will
-                                help users to donate things. ReactJS and syntactically awesome
-                                style sheets (SCSS) were used.</p>
+                            <p>
+                                <span>&lt; React.js, </span>
+                                <span>HTML, </span>
+                                <span>SCSS  /&gt;</span>
+                            </p>
+                            {/*<p>The aim of the project were to create a place where everyone*/}
+                            {/*    could give unnecessary things to trusted institutions.*/}
+                            {/*    The project is a website with a simple form that will*/}
+                            {/*    help users to donate things. ReactJS and syntactically awesome*/}
+                            {/*    style sheets (SCSS) were used.</p>*/}
                         </figcaption>
                     </figure>
                     <figure className="effect-zoe">
-                        <h2>Work in<span>Progress</span></h2>
+                        <h2>Black <span>Chair</span></h2>
                         <img className="workProgress" alt="workProgress" src={progress} />
+                        <span><a href="https://black-chair-gatsby.netlify.app/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faEye}/></a></span>
+
                         <figcaption>
                             <div className="effectSearch">
-                                <h3>Work in <span>Progress</span></h3>
+
+                                <h3>Black <span>Chair</span></h3>
                                 <div className="icons">
-                                    <span><FontAwesomeIcon icon={faCode } /></span>
+
                                     <span><FontAwesomeIcon icon={faEye} /></span>
+                                    <span><a href="https://github.com/JoannaFigula/Black-Chair" target="_blank" rel="noopener noreferrer"><span><FontAwesomeIcon icon={faCode} /></span></a></span>
                                 </div>
                             </div>
-                            <p>Work in progress!</p>
+                            <p>
+                                <span>&lt; Gatsby.js, </span>
+                                <span>HTML, </span>
+                                <span>SCSS  /&gt;</span>
+                            </p>
+
                         </figcaption>
                     </figure>
                 </div>
